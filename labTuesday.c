64ac6e02180290e83fa68567c46e8ff1e9741902
Apu kumar Giri <87590236+apukumar212@users.noreply.github.com>
2022-07-19 21:08:28 +0530

    Add files via upload

---
+++ labTuesday.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+system("");
+printf("%d\n", 28 & 16);
+printf("%d\n", 26 | 18);
+printf("%d\n", 28 ^ 16);
+printf("%d\n", ~45);
+printf("%d\n", ~35);
+printf("%d\n", 61 >> 1);
+printf("%d\n", 18 << 4);
+printf("%d\n", 32 << 14);
+printf("%d\n", 21 << 5);
+printf("%d\n", 14 >> 5);
+printf("%d\n", 18 >> 6);
+printf("%d\n", 10 >> 4);
+printf("%d\n", 22 & 9);
+printf("%d\n", 16 | 4);
+printf("%d\n", 4 ^ 2);
+printf("%d\n", 26 | 10);
+printf("%d\n", 16 | 5);
+printf("%d\n", 21 ^ 6);
+printf("%d\n", ~ 31);
+printf("%d\n", ~ -18);
+printf("%d\n", 32 << 2);
+printf("%d\n", 29 << 6);
+printf("%d\n", 21 << 8);
+printf("%d\n", 16 << 8);
+printf("%d\n", 18 >> 2);
+printf("%d\n", 34 >> 8);
+}
